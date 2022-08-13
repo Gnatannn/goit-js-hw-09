3d911ee1e468a1064b1027d47df36bbd33f08069
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-08-13 18:27:01 +0000

    Deploying to gh-pages from @ Gnatannn/goit-js-hw-09@c47335eba612a71032feb36b5341db7aab025dc5 🚀

---
+++ 01-color-switcher.a9974032.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.setAttribute("disabled","")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled")}));let e=null;
+//# sourceMappingURL=01-color-switcher.a9974032.js.map
