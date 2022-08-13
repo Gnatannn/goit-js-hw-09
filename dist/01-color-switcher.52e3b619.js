c47335eba612a71032feb36b5341db7aab025dc5
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-08-13 21:26:19 +0300

    2

--- dist/01-color-switcher.52e3b619.js
+++ dist/01-color-switcher.52e3b619.js
@@ -503,6 +503,28 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"cYUEh":[function(require,module,exports) {
+const refs = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+    body: document.querySelector("body")
+};
+refs.startBtn.addEventListener("click", onStartClick);
+refs.stopBtn.addEventListener("click", onStopClick);
+let timerId = null;
+function onStartClick() {
+    timerId = setInterval(()=>{
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+    refs.startBtn.setAttribute("disabled", "");
+//   console.log('click start');
+}
+function onStopClick() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute("disabled");
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 },{}]},["gzrwE","cYUEh"], "cYUEh", "parcelRequire3ec5")
 
--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+let timerId = null;
+
+function onStartClick() {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', '');
+  //   console.log('click start');
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
